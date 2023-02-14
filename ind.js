f1dc833827ec6c818d79b919cf6e6a0b6e582975
sansiil <sahanshiludash@gmail.com>
2023-02-15 00:40:03 +0545

    js for nav

---
+++ ind.js
@@ -0,0 +1,10 @@
+function nav(){
+    const navbar=document.getElementById('navbar');
+    
+    if(navbar.style.display ==='block'){
+        navbar.style.display='none';
+    }
+    else{
+        navbar.style.display='block';
+        };
+};
